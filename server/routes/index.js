28e82fb96c7cc1e2cd7f82a476720998ade017e3
Grigory <58467895+naHDop-tech@users.noreply.github.com>
2022-02-06 15:22:56 +0300

    feat: init nestjs (#7)

--- server/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-/* GET home page. */
-router.get("/ping", function (req, res, next) {
-    res.status(200).send("pong");
-});
-
-module.exports = router;
